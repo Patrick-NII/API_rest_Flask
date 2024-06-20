c924927ea65b50a7eb2f7942ebcbb85058087197
NII <patrick@niip.me>
2024-06-20 10:50:53 +0200

    ajout d'un produit dans la base de donné avec post

--- resources.py
+++ resources.py
@@ -1,6 +1,8 @@
+from flask import request
 from flask_restful import Resource
+from marshmallow import ValidationError
 
-from models import Product
+from models import Product, db
 from schemas import ProductSchema
 
 class ProductResources(Resource):
@@ -15,4 +17,18 @@ class ProductResources(Resource):
         else:
             all_products = Product.query.all()  
             return self.product_list_schema.dump(all_products)
-     
+    def post(self):
+        try:
+           new_product_data =  self.product_schema.load(request.json)
+        except ValidationError as err:
+            return {"message":"ValidationError", "errors": err.messages}, 400
+        
+        new_product = Product(
+            name=new_product_data['name'],
+            description=new_product_data['description'],
+            price=new_product_data['price']
+            
+        )
+        db.session.add(new_product)
+        db.session.commit()
+        return self.product_schema.dump(new_product), 201
