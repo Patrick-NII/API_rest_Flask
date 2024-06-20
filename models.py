7b772cbcb99f79a6a9d7846998fee798fbc378fb
NII <patrick@niip.me>
2024-06-20 10:08:37 +0200

    review code

--- models.py
+++ models.py
@@ -1,10 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Float, Integer, String
+from sqlalchemy.orm import declarative_base
 
-base = declarative_base()
+Base = declarative_base()
 
-db = SQLAlchemy(model_class=base)
+db = SQLAlchemy(model_class=Base)
 
-class product(base):
+class Product(Base):
+    
+    __tablename__ = 'product'
     id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=False, nullable=False)
+    description = Column(String, index=False, nullable=False)
+    price = Column(Float, nullable=False)
